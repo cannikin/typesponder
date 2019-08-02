d195ebc1f3f1a93e98fb5b0b77f4d64a0a40d9e4
Rob Cameron <rob.cameron@fastmail.com>
2019-08-02 13:54:11 -0700

    Change attribute name to just "netlify"

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -5,7 +5,7 @@ class Notes extends React.Component {
     const { notes } = this.props
 
     return(
-      <form className="w-50 pl3" name="notes" method="POST" data-netlify="true">
+      <form className="w-50 pl3" name="notes" method="POST" netlify>
         <textarea name="notes" className="w-100 h5 f6 pa3 b--moon-gray mb2" placeholder="Notes" defaultValue={notes || ""} />
         <button type="submit" className="db f5 fw5 bg-pwv-red pv3 w4 white bn br2 center">Save</button>
       </form>
