795f87f50f5f1fc559a415f7b368fa076b0c5b13
Rob Cameron <rob.cameron@fastmail.com>
2019-08-02 15:20:55 -0700

    Refactors form data

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -4,26 +4,29 @@ import endpoints from "../endpoints"
 class Notes extends React.Component {
   constructor(props) {
     super(props)
-
     this.state = {
       saving: false
     }
   }
 
-  onSubmit = (event) => {
-    const formData = {}
+  formData(form) {
+    let formData = {}
 
-    this.setState({ saving: true })
-
-    Array.from(event.target.elements).forEach(element => {
+    Array.from(form.elements).forEach(element => {
       if (element.name) {
         formData[element.name] = element.value
       }
     })
 
+    return formData
+  }
+
+  onSubmit = (event) => {
+    this.setState({ saving: true })
+
     fetch(endpoints.save, {
       method: 'post',
-      body: JSON.stringify(formData)
+      body: JSON.stringify(this.formData(event.target))
     })
       .then(response => ( response.json() ))
       .then(data => {
@@ -40,7 +43,7 @@ class Notes extends React.Component {
     return(
       <form className="w-50 pl3" action={endpoints.save} method="POST" onSubmit={ this.onSubmit }>
         <input type="hidden" name="id" value={ id } />
-        <textarea name="notes" className="w-100 h5 f6 br3 pa3 b--moon-gray mb2" placeholder="Notes" defaultValue={notes || ""} />
+        <textarea name="notes" className="w-100 h5 f6 br3 pa3 b--moon-gray mb2" placeholder="Notes" value={notes || ''} />
         <button type="submit" className={ `db f5 fw5 bg-pwv-red pv3 w4 white bn br2 center pointer ${ saving ? "o-70" : "" }` } disabled={ saving ? true : false }>
           {
             saving
