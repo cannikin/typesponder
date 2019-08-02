ded70f462eefc7c6f64a281cb06a4454e76e1db0
Rob Cameron <rob.cameron@fastmail.com>
2019-08-02 13:16:14 -0700

    Moves responses to function, add simple loading

--- src/App.js
+++ src/App.js
@@ -6,41 +6,23 @@ import logo from './images/logo.svg'
 import Responses from './components/Responses'
 import Detail from './components/Detail'
 
-const RESPONSES = [
-  {
-    id: "abcd",
-    email: "rob.cameron@fastmail.com",
-    created_at: new Date(),
-    answers: [
-      { question: "Name", answer: "Rob Cameron" },
-      { question: "Organization Name", answer: "Cameron Tech" },
-      { question: "Countries your organization operates in", answer: "USA and Canada" },
-      { question: "How would you describe what your organization does to an interested non-expert?", answer: "We make cool stuff" },
-      { question: "What current family planning programming do you have? How does it fit within your overall programming?", answer: "Lorem ipsum dolor amet pitchfork literally craft beer salvia hot chicken. Hoodie master cleanse irony blog chicharrones helvetica pok pok, williamsburg 3 wolf moon mustache kale chips pitchfork edison bulb. Umami chillwave squid cred, trust fund schlitz pug etsy post-ironic retro chartreuse adaptogen coloring book snackwave. Yr la croix sriracha, banjo cred fixie cornhole hammock kickstarter chicharrones.\n\nJianbing la croix aesthetic ugh, raw denim typewriter pork belly coloring book food truck cray. Tumeric narwhal brooklyn, crucifix authentic chicharrones gluten-free mlkshk etsy hot chicken small batch ethical. Helvetica heirloom etsy knausgaard mlkshk. Chicharrones wayfarers authentic, cloud bread synth locavore banjo try-hard poke. Kickstarter raw denim scenester echo park sartorial iPhone, mumblecore locavore keffiyeh hoodie. YOLO tumeric affogato, gochujang banh mi selvage put a bird on it 90's next level swag normcore chia pok pok." }
-    ]
-  },
-  {
-    id: "efgh",
-    email: "jack.cameron@icloud.com",
-    created_at: new Date(),
-    answers: [
-      { question: "Name", answer: "Jack Cameron" },
-      { question: "Organization Name", answer: "Transformers Inc." },
-      { question: "Countries your organization operates in", answer: "USA and Canada" },
-      { question: "How would you describe what your organization does to an interested non-expert?", answer: "Spend all dad's money on Transformers" },
-      { question: "What current family planning programming do you have? How does it fit within your overall programming?", answer: "Lorem ipsum dolor amet pitchfork literally craft beer salvia hot chicken. Hoodie master cleanse irony blog chicharrones helvetica pok pok, williamsburg 3 wolf moon mustache kale chips pitchfork edison bulb. Umami chillwave squid cred, trust fund schlitz pug etsy post-ironic retro chartreuse adaptogen coloring book snackwave. Yr la croix sriracha, banjo cred fixie cornhole hammock kickstarter chicharrones.\n\nJianbing la croix aesthetic ugh, raw denim typewriter pork belly coloring book food truck cray. Tumeric narwhal brooklyn, crucifix authentic chicharrones gluten-free mlkshk etsy hot chicken small batch ethical. Helvetica heirloom etsy knausgaard mlkshk. Chicharrones wayfarers authentic, cloud bread synth locavore banjo try-hard poke. Kickstarter raw denim scenester echo park sartorial iPhone, mumblecore locavore keffiyeh hoodie. YOLO tumeric affogato, gochujang banh mi selvage put a bird on it 90's next level swag normcore chia pok pok." }
-    ]
-  }
-]
-
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { responses: RESPONSES }
+    this.state = { responses: [] }
+  }
+
+  componentWillMount() {
+    fetch('/.netlify/functions/hello')
+      .then(response => response.json())
+      .then(json => this.setState({ responses: json }))
+      .catch()
   }
 
   render() {
     const { responses } = this.state
+    console.info('rendering')
+    console.info(responses)
 
     return (
       <Router>
@@ -54,12 +36,20 @@ class App extends React.Component {
           </header>
           <main className="flex flex-wrap flex-auto-ns">
             <nav className="w-100 w-25-ns mb3 ph3 bb bn-ns b--moon-gray">
-              <Responses list={ responses } />
+              {
+                responses.length
+                ? <Responses list={ responses } />
+                : "Loading..."
+              }
             </nav>
             <section className="w-100 w-75-ns ph3">
               <Route exact path="/" render={() => (
-                <div>
-                  Pick a response to get started
+                <div className="moon-gray">
+                  {
+                    responses.length
+                    ? "Click a response to get started"
+                    : ""
+                  }
                 </div>
               )} />
               <Route path="/responses/:id" render={({ match }) => (
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -8,26 +8,30 @@ class Detail extends React.Component {
   }
 
   render() {
-    const { answers } = this.props.response
+    try {
+      const { answers } = this.props.response
 
-    return(
-      <div className="flex">
-        <dl className="w-50 mt0 mb3 measure">
-          {
-            answers.map((hash, i) => (
-              <div key={ i }>
-                <dt className="f4 fw5 pwv-blue lh-title mb2">{ hash.question }</dt>
-                <dd className="ma0 mb4 pwv-blue o-70 lh-copy">{ this.textFormat(hash.answer) }</dd>
-              </div>
-            ))
-          }
-        </dl>
-        <form className="w-50 pl3" method="post">
-          <textarea className="w-100 h5 f6 pa3 b--moon-gray mb2" placeholder="Notes" />
-          <button type="submit" className="db f5 fw5 bg-pwv-red pv3 w4 white bn br2 center">Save</button>
-        </form>
-      </div>
-    )
+      return(
+        <div className="flex">
+          <dl className="w-50 mt0 mb3 measure">
+            {
+              answers.map((hash, i) => (
+                <div key={ i }>
+                  <dt className="f4 fw5 pwv-blue lh-title mb2">{ hash.question }</dt>
+                  <dd className="ma0 mb4 pwv-blue o-70 lh-copy">{ this.textFormat(hash.answer) }</dd>
+                </div>
+              ))
+            }
+          </dl>
+          <form className="w-50 pl3" method="post">
+            <textarea className="w-100 h5 f6 pa3 b--moon-gray mb2" placeholder="Notes" />
+            <button type="submit" className="db f5 fw5 bg-pwv-red pv3 w4 white bn br2 center">Save</button>
+          </form>
+        </div>
+      )
+    } catch(e) {
+      return null
+    }
   }
 }
 
