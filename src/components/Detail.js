e26cb9fb1260a554b8fbe59e4144e30ac716dce7
Rob Cameron <rob.cameron@fastmail.com>
2019-08-12 15:32:07 -0700

    Linkify answers

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,30 +1,31 @@
 import React from "react"
+import Linkify from "react-linkify"
 import Notes from "./Notes"
 
 export default function Detail({ user, forms, onUserUpdate }) {
+  const { id, notes } = user || {}
+  const answers = ((user || {}).responses || []).map(r => r.answers)[0]
+  const questions = (forms || []).map(f => f.questions).flat()
+
   function textFormat(text) {
     return String(text).split('\n').map((item, key) => {
       return <span key={key}>{item}<br/></span>
     })
   }
 
-  function findQuestion(questions, answer) {
+  function findQuestion(answer) {
     return questions.find(q => q.id === answer.questionId)
   }
 
   if (user && forms) {
-    const { id, notes } = user
-    const answers = user.responses.map(r => r.answers)[0]
-    const questions = forms.map(f => f.questions).flat()
-    
     return(
       <div className="relative">
         <dl className="w-50-ns mt0 mb3 measure">
           {
             answers.map(answer => (
               <div key={ answer.questionId }>
-                <dt className="f4 fw5 pwv-blue lh-title mb2">{ findQuestion(questions, answer).text }</dt>
-                <dd className="ma0 mb4 pwv-blue o-70 lh-copy">{ textFormat(answer.text) }</dd>
+                <dt className="f4 fw5 pwv-blue lh-title mb2">{ findQuestion(answer).text }</dt>
+                <Linkify properties={{ target: "_blank" }}><dd className="ma0 mb4 pwv-blue o-70 lh-copy">{ textFormat(answer.text) }</dd></Linkify>
               </div>
             ))
           }
