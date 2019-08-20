f36246eacd7b296b4f277b4ecd3c7dbf65ffa81a
Rob Cameron <rob.cameron@fastmail.com>
2019-08-20 13:28:15 -0700

    Fix truncated answers, only truncate file_upload types

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Linkify from "react-linkify"
 import Notes from "./Notes"
+import endpoints from "../endpoints";
 
 export default function Detail({ user, forms, onUserUpdate }) {
   const { id, notes } = user || {}
@@ -19,6 +20,16 @@ export default function Detail({ user, forms, onUserUpdate }) {
     return question || { text: `QuestionId ${answer.questionId} not found` }
   }
 
+  function answerClassName(answer) {
+    let classes = ["ma0", "mb4", "pwv-blue", "o-70", "lh-copy"]
+
+    if (findQuestion(answer).type === "file_upload") {
+      classes.concat(["nowrap", "truncate", "o-hidden"])
+    }
+
+    return classes.join(" ")
+  }
+
   if (user && forms) {
     return(
       <div className="relative">
@@ -27,7 +38,7 @@ export default function Detail({ user, forms, onUserUpdate }) {
             answers.map((answer, i) => (
               <div key={ i }>
                 <dt className="f4 fw5 pwv-blue lh-title mb2">{ findQuestion(answer).text }</dt>
-                <dd className="ma0 mb4 pwv-blue o-70 lh-copy nowrap truncate o-hidden">
+                <dd className={ answerClassName(answer) } >
                   <Linkify properties={{ target: "_blank" }}>
                     { textFormat(answer.text) }
                   </Linkify>
