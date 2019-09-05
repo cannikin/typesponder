46dad8dcf54e68553138c850545355af0fd90a9a
Rob Cameron <rob.cameron@fastmail.com>
2019-09-05 12:57:16 -0700

    Remove left column when printing

--- src/App.js
+++ src/App.js
@@ -1,59 +1,58 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import "./styles/app.sass"
-import logo from "./images/logo.svg"
-import moment from "moment"
+import "./styles/app.sass";
+import logo from "./images/logo.svg";
+import moment from "moment";
 
-import endpoints from "./endpoints"
-import BlankSlate from "./components/BlankSlate"
-import Responses from "./components/Responses"
-import Detail from "./components/Detail"
-import NewResults from "./components/NewResults"
-import ResultCount from "./components/ResultCount"
+import endpoints from "./endpoints";
+import BlankSlate from "./components/BlankSlate";
+import Responses from "./components/Responses";
+import Detail from "./components/Detail";
+import NewResults from "./components/NewResults";
+import ResultCount from "./components/ResultCount";
 
 export default function App() {
+  const NEW_USER_INTERVAL_SECONDS = 60;
 
-  const NEW_USER_INTERVAL_SECONDS = 60
-
-  const [users, setUsers] = useState([])
-  const [forms, setForms] = useState([])
-  const [newResults, setNewResults] = useState(0)
+  const [users, setUsers] = useState([]);
+  const [forms, setForms] = useState([]);
+  const [newResults, setNewResults] = useState(0);
 
   // gets all data for the app
   async function getData() {
-    const response = await fetch(endpoints.getUsers)
-    const json = await response.json()
+    const response = await fetch(endpoints.getUsers);
+    const json = await response.json();
 
-    return json
+    return json;
   }
 
   // replaces a single user with `userData` and updates `users` state
   function updateUser(userData) {
     const updatedUsers = users.map(user => {
       if (userData.id === user.id) {
-        return userData
+        return userData;
       } else {
-        return user
+        return user;
       }
-    })
+    });
 
-    setUsers(updatedUsers)
+    setUsers(updatedUsers);
   }
 
   // sorts users based on createdAt date, descending
   function sortUsers(users) {
     return users.sort((a, b) => {
       if (moment(a.createdAt).unix() < moment(b.createdAt).unix()) {
-        return 1
+        return 1;
       } else {
-        return -1
+        return -1;
       }
-    })
+    });
   }
 
   // counts number of responses for all users
   function responseCount(users) {
-    return users.reduce((count, user) => count + user.responses.length, 0)
+    return users.reduce((count, user) => count + user.responses.length, 0);
   }
 
   // gets data and populates state
@@ -61,61 +60,68 @@ export default function App() {
     if (!users.length) {
       // eslint-disable-next-line no-inner-declarations
       async function get() {
-        let json = await getData()
-        setForms(json.forms)
-        setUsers(sortUsers(json.users))
+        let json = await getData();
+        setForms(json.forms);
+        setUsers(sortUsers(json.users));
       }
-      get()
+      get();
     }
-  })
+  });
 
   // periodically check for new responses and set `newResults` state if new data is available
   // used https://overreacted.io/making-setinterval-declarative-with-react-hooks/ for help
   useEffect(() => {
     let id = setInterval(async () => {
       if (users.length) {
-        const response = await fetch(endpoints.responsesCount)
-        const json = await response.json()
-        const newResponsesCount = json.responsesCount - responseCount(users)
+        const response = await fetch(endpoints.responsesCount);
+        const json = await response.json();
+        const newResponsesCount = json.responsesCount - responseCount(users);
 
         if (newResponsesCount) {
-          setNewResults(newResponsesCount)
+          setNewResults(newResponsesCount);
         }
       }
-    }, NEW_USER_INTERVAL_SECONDS * 1000)
+    }, NEW_USER_INTERVAL_SECONDS * 1000);
 
-    return () => clearInterval(id)
-  })
+    return () => clearInterval(id);
+  });
 
   return (
     <Router>
       <div className="app sans-serif">
         <header className="App-header ph3 mb4 flex bb b--moon-gray">
           <h1 className="ma0 f4 w-third pv3">
-            <img src={ logo } alt="logo" className="w2 v-mid mr2" />
-            <Link to="/" className="no-underline pwv-blue">Typesponder</Link>
+            <img src={logo} alt="logo" className="w2 v-mid mr2" />
+            <Link to="/" className="no-underline pwv-blue">
+              Typesponder
+            </Link>
           </h1>
           <div className="w-third tc">
-            <NewResults count={ newResults } />
+            <NewResults count={newResults} />
           </div>
           <h3 className="mt1 mb0 f6 pv3 fw4 w-third tr silver">
-            <ResultCount users={ users } count={ responseCount } />
+            <ResultCount users={users} count={responseCount} />
           </h3>
         </header>
         <main className="flex flex-wrap flex-auto-ns">
-          <nav className="w-100 w-25-ns mb3 ph3 bb bn-ns b--moon-gray">
-            <Responses users={ users } forms={ forms } />
+          <nav className="w-100 w-25-ns mb3 ph3 bb bn-ns b--moon-gray no-print">
+            <Responses users={users} forms={forms} />
           </nav>
           <section className="w-100 w-75-ns ph3">
-            <Route exact path="/" render={() => (
-              <BlankSlate />
-            )} />
-            <Route path="/users/:id" render={({ match }) => (
-              <Detail user={ users.find(r => r.id === parseInt(match.params.id)) } forms={ forms } onUserUpdate={ updateUser } />
-            )} />
+            <Route exact path="/" render={() => <BlankSlate />} />
+            <Route
+              path="/users/:id"
+              render={({ match }) => (
+                <Detail
+                  user={users.find(r => r.id === parseInt(match.params.id))}
+                  forms={forms}
+                  onUserUpdate={updateUser}
+                />
+              )}
+            />
           </section>
         </main>
       </div>
     </Router>
-  )
+  );
 }
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,71 +1,85 @@
-import React, { useState } from "react"
-import endpoints from "../endpoints"
+import React, { useState } from "react";
+import endpoints from "../endpoints";
 
 export default function Notes({ id, notes, onUpdate }) {
-  const [saving, setSaving] = useState(false)
-  const [input, setInput] = useState(notes === " " ? "" : notes)
-  const [prevId, setPrevId] = useState(id)
+  const [saving, setSaving] = useState(false);
+  const [input, setInput] = useState(notes === " " ? "" : notes);
+  const [prevId, setPrevId] = useState(id);
 
   // component won't re-render when changing responses so it always shows the notes from the first
   // picked response. force the input value to change here when the ID changes
   if (id !== prevId) {
-    setInput(notes === " " ? "" : notes)
-    setPrevId(id)
+    setInput(notes === " " ? "" : notes);
+    setPrevId(id);
   }
 
   function formData(form) {
-    let formData = {}
+    let formData = {};
 
     Array.from(form.elements).forEach(element => {
       if (element.name) {
-        formData[element.name] = element.value
+        formData[element.name] = element.value;
       }
-    })
+    });
 
-    return formData
+    return formData;
   }
 
   function saveButtonLabel() {
-    return saving ? "Saving..." : "Save"
+    return saving ? "Saving..." : "Save";
   }
 
   function saveButtonClass() {
-    return saving ? "o-70" : ""
+    return saving ? "o-70" : "";
   }
 
   function onChange(event) {
-    setInput(event.target.value)
+    setInput(event.target.value);
   }
 
   function onSubmit(event) {
-    setSaving(true)
+    setSaving(true);
 
     fetch(endpoints.save, {
       method: "post",
       body: JSON.stringify(formData(event.target))
-    }).then(response => response.json()).then((user) => {
-      onUpdate(user)
-      setSaving(false)
     })
+      .then(response => response.json())
+      .then(user => {
+        onUpdate(user);
+        setSaving(false);
+      });
 
-    event.preventDefault()
+    event.preventDefault();
   }
 
-  return(
-    <form className="w-third-ns mt5-ns pl3-ns fixed-ns top-2 right-2" action={endpoints.save} method="POST" onSubmit={ onSubmit }>
-      <input type="hidden" name="id" value={ id } />
-      <textarea
-        name="notes"
-        className="w-100 h5 f6 br3 pa3 b--moon-gray mb2"
-        placeholder="Notes"
-        value={ input || '' }
-        onChange={ onChange } />
-      <button
-        type="submit"
-        className={ `db f5 fw5 bg-pwv-red pv3 w4 white bn br2 center pointer mb3 ${ saveButtonClass() }` }
-        disabled={ saving }>
-        { saveButtonLabel() }
-      </button>
-    </form>
-  )
+  return (
+    <>
+      <form
+        className="w-third-ns mt5-ns pl3-ns fixed-ns top-2 right-2 no-print"
+        action={endpoints.save}
+        method="POST"
+        onSubmit={onSubmit}>
+        <input type="hidden" name="id" value={id} />
+        <textarea
+          name="notes"
+          className="w-100 h5 f6 br3 pa3 b--moon-gray mb2"
+          placeholder="Notes"
+          value={input || ""}
+          onChange={onChange}
+        />
+        <button
+          type="submit"
+          className={`db f5 fw5 bg-pwv-red pv3 w4 white bn br2 center pointer mb3 no-print ${saveButtonClass()}`}
+          disabled={saving}>
+          {saveButtonLabel()}
+        </button>
+      </form>
+      <div class="print-only">
+        <hr />
+        <h2 class="f4 fw5">Notes</h2>
+        <div class="courier">{notes}</div>
+      </div>
+    </>
+  );
 }
