c3d4323a121f1362495bb72e6815e50d2fbf8f8c
Rob Cameron <rob.cameron@fastmail.com>
2019-10-02 10:48:02 -0700

    Update get-users to only pull a limited number of results and make subsequent calls to paginate

--- functions/get-users.js
+++ functions/get-users.js
@@ -1,16 +1,29 @@
-const AWS = require('./dynamo_connect')
+// Returns all forms and a limit of 25 users. Pass ?lastUser=123 for the starting point for the next
+// 25 users
+
+const AWS = require("./dynamo_connect");
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = (event, context, callback) => {
+  console.log(event.queryStringParameters.lastUser);
+  const lastUser = event.queryStringParameters.lastUser;
 
-  docClient.scan({ TableName: "users" }, (err, users) => {
-    docClient.scan({ TableName: "forms" }, (err, forms) => {
-      callback(null , {
-        statusCode: 200,
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ forms: forms.Items, users: users.Items })
+  let userParams = {
+    TableName: "users",
+    Limit: 2,
+    ExclusiveStartKey: lastUser ? { id: parseInt(lastUser) } : null
+  };
+
+  docClient.scan(userParams, (err, users) => {
+    callback(null, {
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        users: users.Items,
+        lastUser: users.LastEvaluatedKey
       })
-    })
-  })
+    });
+  });
+};
 
-}
+// If using limit then resultset will return `LastEvaluatedKey`, give that to the next query in the form of `ExclusiveStartKey`
