51b6e96a6cfd16e2bf66592fc0ea606018027060
Rob Cameron <rob.cameron@fastmail.com>
2019-10-02 12:00:11 -0700

    Update responses-count to actually do a count

--- functions/responses-count.js
+++ functions/responses-count.js
@@ -2,12 +2,12 @@ const AWS = require("./dynamo_connect");
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = (event, context, callback) => {
-  docClient.scan({ TableName: "users" }, (err, users) => {
+  docClient.scan({ TableName: "users", Select: "COUNT" }, (err, result) => {
     callback(null, {
       statusCode: 200,
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        responsesCount: users.Items.reduce((count, user) => count + user.responses.length, 0)
+        responsesCount: result.Count
       })
     });
   });
